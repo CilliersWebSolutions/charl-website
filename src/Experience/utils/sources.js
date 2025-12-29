11e6671aeef36db995625dfc23f6dbb0e60fe078
Juan <cillierswebsolutions@gmail.com>
2025-12-29 19:25:46 +0100

    fix texture
    redid textures

--- src/Experience/utils/sources.js
+++ src/Experience/utils/sources.js
@@ -2,13 +2,13 @@ export default [
     {
         name: 'liningModel',
         type: 'gltfModel',
-        path: 'https://cdn.prod.website-files.com/68a43ca43fc6c140cf3f82c2/69525d39c296cb39acb4ffa1_landfill_justSquare.glb.txt'
+        path: 'https://cdn.prod.website-files.com/68a43ca43fc6c140cf3f82c2/6952c750a40f89c97f68f01b_landfill_justSquare.glb.txt'
     },
 
     {
         name: 'liningTexture',
         type: 'texture',
-        path: 'https://cdn.prod.website-files.com/68a43ca43fc6c140cf3f82c2/69525e6c16ea93315ad712f4_baked-final.jpg'
+        path: 'https://cdn.prod.website-files.com/68a43ca43fc6c140cf3f82c2/6952c795cb1b902dfc082c89_baked-final.jpg'
     },
 
     {
