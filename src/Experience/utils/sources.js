eae2bfb06489d9d83a163be124182fa3bc548ccb
Juan <cillierswebsolutions@gmail.com>
2026-01-28 11:03:58 +0100

    update texture
    made textures brighter and fixed the repeating textures

--- src/Experience/utils/sources.js
+++ src/Experience/utils/sources.js
@@ -8,7 +8,7 @@ export default [
     {
         name: 'liningTexture',
         type: 'texture',
-        path: 'https://cdn.prod.website-files.com/68a43ca43fc6c140cf3f82c2/69650d80e8574eeee4306533_baked-albedo2k-v3.avif'
+        path: 'https://cdn.prod.website-files.com/6973775c7a3601a9f840ee21/6979de591b6aff71916f8f94_baked-albedo2k-v4.avif'
     },
 
     // Normal texture removed to reduce payload
